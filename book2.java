9fef28353823ad4a0ddca7ce7b50303d93d40d02
hanumakaushik1523 <117442372+hanumakaushik1523@users.noreply.github.com>
2022-12-08 22:54:29 -0800

    Update book2.java

--- book2.java
+++ book2.java
@@ -1,17 +1,19 @@
 import java.util.*;
 import java.lang.*;
-class Book
+import java.util.Scanner;
+class book
 {
-      String name,author; 
+      String name,author;
       int price, num_pages;  
      void getval()
      {
      
        Scanner sc=new Scanner(System.in);
-       System.out.println("Enter book name");
-       name=sc.next();
+       System.out.println("Enter book name:");
+       name=sc.nextLine();
+
        System.out.println("Enter author name");
-       author=sc.next();
+       author=sc.nextLine();
        System.out.println("Enter price ");
        price=sc.nextInt();
        System.out.println("Enter No. of pages");
@@ -19,28 +21,33 @@ class Book
      }
     void display()
      {
-         System.out.println("Details of the book");
-         System.out.println("name of the book "+name);
-         System.out.println("author of book "+author);
-         System.out.println("book price "+price);
-         System.out.println("number of pages "+num_pages);
+         System.out.println("Details of the book:");
+         System.out.println("name of the book :"+name);
+         System.out.println("author of book: "+author);
+         System.out.println("book price :"+price);
+         System.out.println("number of pages :"+num_pages);
      }
 }
-class Book1
-{ 
+class Main
+{
   public static void main(String args[])
-   { 
-     Book ob[]=new Book [3]; 
-     for(int i=0;i<3;i++)
-     { 
-        ob[i]=new Book();
+   {
+     int n;
+     Scanner sc=new Scanner(System.in) ;
+     System.out.println("enter the number of books:");
+     n=sc.nextInt();
+     
+     book ob[]=new book[n];
+     for(int i=0;i<n;i++)
+     {
+        ob[i]=new book();
      }
-     for(int j=0;j<3;j++)
-     { 
+     for(int j=0;j<n;j++)
+     {
         ob[j].getval();
      }
-     for(int j=0;j<3;j++)
-     { 
+     for(int j=0;j<n;j++)
+     {
         ob[j].display();
      }
    }
